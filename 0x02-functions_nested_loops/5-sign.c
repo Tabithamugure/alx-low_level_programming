8502998e2edffa1580ca7d3c073863fee09a8cc1
Tabithamugure <tarbiemugure@gmail.com>
2022-12-13 02:13:46 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * 0 of number is zero
- * -1 if number is less than zero
+ * Return: 0
  */
 int print_sign(int n)
 {
