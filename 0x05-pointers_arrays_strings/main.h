12c6f730f135ecedac190854dc52dcb0ac68d365
Tabithamugure <tarbiemugure@gmail.com>
2022-12-19 04:22:12 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * void prototypes()
@@ -18,4 +18,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 
-#endif /"MAIN_H"/
+#endif /*MAIN_H*/
