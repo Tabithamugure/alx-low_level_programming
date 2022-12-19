607490d2486b7e215c34e10d482d8fefbd6ab208
Tabithamugure <tarbiemugure@gmail.com>
2022-12-19 03:53:05 -0800

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN.H
 #define MAIN.H
 
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,4 +18,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 
-#endif
+#endif /"MAIN_H"/
