462c621ed8de6173bd5f198685bbf7e381e2735f
Tabithamugure <tarbiemugure@gmail.com>
2023-01-05 05:18:40 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,15 @@
-/*
- * File: 1-print_rev_recursion.c
- * Auth: Tarbie
- */
-
 #include "main.h"
-
 /**
- * _print_rev recursion - Prints a string in reverse.
- * @s: The string to be printed.
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer block of memory to fill
+ * Return: void
  */
+
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s) /*if statement*/
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+	_print_rev_recursion(s + 1); /*add s*/
+	_putchar(*s); /*print s*/
 	}
 }
