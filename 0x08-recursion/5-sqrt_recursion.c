452580d26247a08a62c412926691127412ded53b
Tabithamugure <tarbiemugure@gmail.com>
2023-01-05 05:32:08 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,15 +8,17 @@
 
 int _evaluate(int i, int n)
 {
-	/*evaluate function*/
-	if (n == 0 || n == 1)
-		return (n);
+/*evaluate function*/
+if (n == 0 || n == 1)
+return (n);
 
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
+else if (i * i < n)
+return (_evaluate(i + 1, n));
 
-	else if (i * i == n) /*condition base*/
-		return (i);
+else if (i * i == n) /*condition base*/
+return (i);
 
-	return (-1);
+return (-1);
+
+return (-1);
 }
